e037d16e247027d43f9d5935f21e4bdd31d318ef
yihong0618 <zouzou0208@gmail.com>
2020-09-24 23:55:56 +0800

    fix: windows \r?

--- kb_gen.sh
+++ kb_gen.sh
@@ -1,7 +1,19 @@
 # Split bash string by newline characters
-IFS=$'\n' read -rd '' -a title <<<"$1"
+# must \r\n
+IFS=$'\r\n' read -rd '' -a title <<<"$1"
 
-echo yes | kb import kb_data/kb_data.kb.tar.gz
+if [ -e kb_data/kb_data.kb.tar.gz ]
+then
+    echo yes | kb import kb_data/kb_data.kb.tar.gz
+else
+    echo 'no data'
+fi
 
 kb add -t "${title}" -b "$1" -c "$2"
+
+if ! [ -e kb_data ]
+then 
+    mkdir kb_data
+fi
+
 kb export --file kb_data/kb_data
