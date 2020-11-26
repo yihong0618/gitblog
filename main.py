a8c7ec02fdc395eeedd4a06643619f84737f54c4
yihong0618 <zouzou0208@gmail.com>
2020-11-26 09:31:30 +0800

    add: backup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from github import Github
 from github.Issue import Issue
 import argparse
@@ -7,6 +8,7 @@ MD_HEAD = """## Gitblog
 My personal blog using issues and GitHub Actions
 """
 
+BACKUP_DIR = "BACKUP"
 ANCHOR_NUMBER = 5
 TOP_ISSUES_LABELS = ["Top"]
 TODO_ISSUES_LABELS = ["TODO"]
@@ -32,7 +34,7 @@ def get_repo(user: Github, repo: str):
     return user.get_repo(repo)
 
 
-def parseTODO(issue):
+def parse_TODO(issue):
     body = issue.body.splitlines()
     todo_undone = [l for l in body if l.startswith("- [ ] ")]
     todo_done = [l for l in body if l.startswith("- [x] ")]
@@ -74,7 +76,7 @@ def add_md_todo(repo, md, me):
         md.write("## TODO\n")
         for issue in todo_issues:
             if isMe(issue, me):
-                todo_title, todo_list = parseTODO(issue)
+                todo_title, todo_list = parse_TODO(issue)
                 md.write("TODO list from " + todo_title + "\n")
                 for t in todo_list:
                     md.write(t + "\n")
@@ -143,7 +145,17 @@ def add_md_label(repo, md, me):
                 md.write("\n")
 
 
-def main(token, repo_name):
+def get_to_generate_issues(repo, dir_name, issue_number=None):
+    md_files = os.listdir(dir_name)
+    generated_issues_numbers = [int(i.split("_")[0]) for i in md_files if i.split("_")[0].isdigit()]
+    to_generate_issues = [i for i in list(repo.get_issues()) if int(i.number) not in generated_issues_numbers]
+    if issue_number:
+        to_generate_issues.append(repo.get_issue(int(issue_number)))
+    print(to_generate_issues)
+    return to_generate_issues
+
+
+def main(token, repo_name, issue_number=None, dir_name=BACKUP_DIR):
     user = login(token)
     me = get_me(user)
     repo = get_repo(user, repo_name)
@@ -152,10 +164,31 @@ def main(token, repo_name):
     for func in [add_md_top, add_md_recent, add_md_label, add_md_todo]:
         func(repo, "README.md", me)
 
+    to_generate_issues = get_to_generate_issues(repo, dir_name, issue_number)
+    for issue in to_generate_issues:
+        save_issue(issue, me, dir_name)
+    # issues = repo.get_issues(to_generate_issues_numbers)
+
+
+def save_issue(issue, me, dir_name=BACKUP_DIR):
+    md_name = os.path.join(dir_name, f"{issue.number}_{issue.title.replace(' ', '.')}.md")
+    with open(md_name, "w") as f:
+        f.write(f"# [{issue.title}]({issue.html_url})\n\n")
+        f.write(issue.body)
+        if issue.comments:
+            for c in issue.get_comments():
+                if isMe(c, me):
+                    f.write("\n\n---\n\n")
+                    f.write(c.body)
+
 
 if __name__ == "__main__":
+    if not os.path.exists(BACKUP_DIR):
+        os.mkdir(BACKUP_DIR)
     parser = argparse.ArgumentParser()
     parser.add_argument("github_token", help="github_token")
     parser.add_argument("repo_name", help="repo_name")
+    parser.add_argument("issue_number", help="issue_number", default=None)
     options = parser.parse_args()
-    main(options.github_token, options.repo_name)
+    print(options.issue_number)
+    main(options.github_token, options.repo_name, options.issue_number)
