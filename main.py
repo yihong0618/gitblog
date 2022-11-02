e47d6e772930586705f0e0f76ded13fd57a91b7b
Tsing Yun <imtsingyun@gmail.com>
2022-11-02 22:09:06 +0800

    sort lables by description info if it exists, otherwise sort by name
    for example, we can let the description start with a number (1#Java, 2#Docker, 3#K8s, etc.)

--- main.py
+++ main.py
@@ -188,6 +188,11 @@ def add_md_header(md, repo_name):
 
 def add_md_label(repo, md, me):
     labels = get_repo_labels(repo)
+
+    # sort lables by description info if it exists, otherwise sort by name, 
+    # for example, we can let the description start with a number (1#Java, 2#Docker, 3#K8s, etc.)
+    labels = sorted(labels, key=lambda x: (x.description is None, x.description == "", x.description, x.name))
+
     with open(md, "a+", encoding="utf-8") as md:
         for label in labels:
 
