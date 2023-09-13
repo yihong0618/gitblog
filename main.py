52c41589f5f938e8e0c39a7f71c8191a4acbb6dc
yihong0618 <zouzou0208@gmail.com>
2023-09-13 19:28:06 +0800

    fix: link things
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ def add_md_firends(repo, md, me):
     s = markdown.markdown(s, output_format="html", extensions=["extra"])
     with open(md, "a+", encoding="utf-8") as md:
         md.write(
-            f"## [友情链接](https://github.com/{str(me)}/gitblog/{friends_issue_number})\n"
+            f"## [友情链接](https://github.com/{str(me)}/gitblog/issues/{friends_issue_number})\n"
         )
         md.write(s)
         md.write("\n\n")
