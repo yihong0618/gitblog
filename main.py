fe4a4a3e8daa14578f17c0fdf28f6d90cbedea1b
yihong0618 <zouzou0208@gmail.com>
2024-08-21 16:26:00 +0800

    fix: not by ai
    Signed-off-by: yihong0618 <zouzou0208@gmail.com>

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## [Gitblog](https://yihong0618.github.io/gitblog/)
 My personal blog([About Me](https://github.com/yihong0618/gitblog/issues/282)) using issues and GitHub Actions (随意转载，无需署名)
+![image](https://github.com/user-attachments/assets/a168bf11-661e-4566-b042-7fc9544de528)
 [RSS Feed](https://raw.githubusercontent.com/{repo_name}/master/feed.xml)
 """
 
